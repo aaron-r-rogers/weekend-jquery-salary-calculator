f6109af801d05acab05428e36f720ef8ba45c176
aaron-r-rogers <rogers.aaron.r@gmail.com>
2021-12-11 10:15:31 -0600

    Function to add employe built, debugging

--- salarycalc.js
+++ salarycalc.js
@@ -1,5 +1,25 @@
 $(document).ready(readyNow);
 
 function readyNow() {
+    console.log(`Ready now.`);
+    $('#form').submit(onAddEmployee);
+}
 
+function onAddEmployee(event) {
+    event.preventDefault();
+
+    let firstName = $('#firstName').val();
+    let lastName = $('#lastName').val();
+    let iD = Number($('#iD').val());
+    let title = $('#title').val();
+    let annualSalary = Number($('#annualSalary').val());
+
+    let newEmployee = {
+        firstName: firstName,
+        lastName: lastName,
+        iD: iD,
+        title: title,
+        annualSalary: annualSalary
+    };
+    console.log(`New employee: ${newEmployee}`);
 }
